01a6b5d7c7d5b30bdc6696d67416deefd56849a6
ihenik <idan7h@gmail.com>
2022-09-21 17:06:08 -0400

    Added end points

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -4,6 +4,12 @@ app = express();
 app.get('/', function (req, res) {
   res.send('Hello World!\n');
 });
+app.get('/mamik', function (req, res) {
+  res.send('Hey Babe\n');
+});
+app.get('/cuz', function (req, res) {
+  res.send('Waddup Bro\n');
+});
 
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
