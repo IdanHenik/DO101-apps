32200efbda9acf51090f18565a002cafc219997f
ihenik <idan7h@gmail.com>
2022-09-22 02:49:45 -0400

    Version 2

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -2,14 +2,9 @@ var express = require('express');
 app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!\n');
+  res.send('Version 2\n');
 });
-app.get('/mamik', function (req, res) {
-  res.send('Hey Babe\n');
-});
-app.get('/cuz', function (req, res) {
-  res.send('Waddup Bro\n');
-});
+
 
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
